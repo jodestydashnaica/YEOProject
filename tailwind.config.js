0684ddeb9399b26fc709de0b6e560a6ad73eb443
Jodesty Dash-naica <109391814+jodestydashnaica@users.noreply.github.com>
2023-01-30 20:29:54 -0500

    Ajout initial des fichiers du site web
    This site is an online portal for an organization [Youth empowerment organization], which aims to present to members and visitors their mission and the different activities they organize. The site will include a home page that will present general information about the organization and its programs, a resources section through which members and visitors will find relevant links which will help them find additional information and a contact section which will allow them to contact the organization.

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    
+    extend: {
+      colors: {
+        'Bleu': '#12327B'
+      }
+    },
+  },
+  plugins: [],
+}
